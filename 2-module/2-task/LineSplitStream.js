0494b142320a88b5e0f8c70aeed8a441ad7496bc
ea.khorina <Khorina.E.A@sberbank.ru>
2020-05-20 11:49:56 +0300

    Add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -7,9 +7,20 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const rowEndIndex = chunk.indexOf(os.EOL);
+    this.nextRows += chunk.splice(rowEndIndex);
+    callback(null, chunk.splice(0, rowEndIndex));
   }
 
   _flush(callback) {
+    while (this.nextRows.length) {
+      const rowEndIndex = this.nextRows.indexOf(os.EOL);
+      if (rowEndIndex === -1) {
+        return;
+      }
+      this.nextRows = this.nextRows.splice(rowEndIndex);
+      callback(null, this.this.nextRows.splice(0, rowEndIndex));
+    }
   }
 }
 
