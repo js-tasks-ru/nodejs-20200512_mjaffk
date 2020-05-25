3f30acac076ad426d21a6043872cbcb8b050625f
ea.khorina <Khorina.E.A@sberbank.ru>
2020-05-25 11:50:20 +0300

    Add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,24 @@
-const url = require('url');
 const http = require('http');
-const path = require('path');
+const getResponse = require('./getResponse');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
-    case 'GET':
-
+    case 'GET': {
+      getResponse(req, res);
       break;
+    }
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
+});
+
+server.on('error', (req, res) => {
+  res.statusCode = 500;
+  res.end('Internal Server Error');
 });
 
 module.exports = server;
