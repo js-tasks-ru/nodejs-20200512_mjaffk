f314547d7a01c5968c5e73f8a9ad6b51a1e2b71e
ea.khorina <Khorina.E.A@sberbank.ru>
2020-05-31 00:31:51 +0300

    Add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,21 @@
+const Product = require('../models/Product');
+
+const convertProduct = ({_id: id, title, description, price, category, subcategory, images}) => ({
+  id,
+  title,
+  images,
+  category,
+  subcategory,
+  price,
+  description,
+});
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query || {};
+  const products = await Product
+      .find({$text: {$search: query}}, {score: {$meta: 'textScore'}})
+      .sort({score: {$meta: 'textScore'}});
+
+  ctx.body = {products: products ? products.map(convertProduct) : []};
+  return next();
 };
