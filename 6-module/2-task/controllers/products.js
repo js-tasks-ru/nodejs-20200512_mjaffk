e08c817872551037e0996ae1e98634df3cbfae39
ea.khorina <Khorina.E.A@sberbank.ru>
2020-05-30 22:36:56 +0300

    Add solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,61 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+const convertProduct = ({_id: id, title, description, price, category, subcategory, images}) => ({
+  id,
+  title,
+  images,
+  category,
+  subcategory,
+  price,
+  description,
+});
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const {subcategory: subcategoryId} = ctx.query || {};
+  if (!subcategoryId) {
+    return next();
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(subcategoryId)) {
+    ctx.throw(400, 'Invalid subcategoryId');
+  }
+  ctx.subcategoryId = subcategoryId;
+  return next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const {subcategoryId} = ctx;
+
+  const products = subcategoryId ?
+      await Product.find({subcategory: new mongoose.Types.ObjectId(subcategoryId)}) :
+      await Product.find({});
+
+  if (!products || !products.length) {
+    ctx.body = {
+      products: [],
+    };
+    return next;
+  }
+  ctx.body = {products: products.map(convertProduct)};
+  return next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const {id: productId} = ctx.params;
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    ctx.throw(400, 'Invalid productId');
+  }
+  const product = await Product.findById(new mongoose.Types.ObjectId(productId));
+  if (!product) {
+    ctx.throw(404, 'Product not found');
+    ctx.body = {
+      product: {},
+    };
+  }
+  ctx.body = {
+    product: convertProduct(product),
+  };
+  return next();
 };
 
