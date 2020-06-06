9d858eacb2b3182b27a3713f3300e663d25061c4
ea.khorina <Khorina.E.A@sberbank.ru>
2020-06-06 16:50:46 +0300

    Fix lint error for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,7 +1,6 @@
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-
   if (!email) {
     return done(null, false, 'Не указан email');
   }
